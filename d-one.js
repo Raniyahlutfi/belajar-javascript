4a531f5b48a5d6f5f4166a737e997cbd19dd4793
Raniyah <raniyahlutfi3@student.ub.ac.id>
2023-06-26 23:08:05 +0700

    first commit

---
+++ d-one.js
@@ -0,0 +1,12 @@
+console.log("Hello World!")
+
+let namaSaya = "Raniyah Lutfi"
+console.log(namaSaya)
+namaSaya = 13
+console.log(namaSaya)
+
+if(namaSaya == 14){
+    console.log("MUDA")
+}else{
+    console.log("cantik")
+}
---
+++ quiz-d-one.js
@@ -0,0 +1,7 @@
+let x = 13
+
+if(x % 2 == 0){
+    console.log("Genap Brooooo")
+}else{
+    console.log("Ganjil Bestie")
+}
